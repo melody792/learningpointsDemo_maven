4b6a5f5b222f91f9399e5e65854b1ed8b384920f
pzhbao <pzhbao@cn.ibm.com>
2018-12-11 13:27:30 +0800

    temp

---
+++ src/main/java/com/pzb/rxjava/rxJava_studio/Demo.java
@@ -0,0 +1,42 @@
+package com.pzb.rxjava.rxJava_studio;
+
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class Demo {
+    public static void main(String[] args) {
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+                if (!e.isDisposed()) {
+                    e.onNext("test");
+                    e.onComplete();
+                }
+            }
+        }).subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable disposable) {
+                System.out.println("onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println("onNext:  "+ s);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        });
+    }
+}
